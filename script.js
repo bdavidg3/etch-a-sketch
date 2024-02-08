5e719bd4dfccd09f404eabb6bcc4b101dc62b8c9
bdavidg3 <82306261+bdavidg3@users.noreply.github.com>
2024-02-07 21:10:27 -0300

    feat: adds grid coloring if the mouse is over

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 gridContainer = document.querySelector('.grid-container');
-let a=9;
+let a=15;
 
 for (let i=1;i<=a*a;i++){
     const gridItem = document.createElement('div');
@@ -7,4 +7,8 @@ for (let i=1;i<=a*a;i++){
     gridContainer.appendChild(gridItem);
     gridItem.style.width=`calc(100% / ${a})`;
     gridItem.style.heigth=`calc(100% / ${a})`;
+    gridItem.addEventListener('mouseover',()=>{
+    gridItem.style.backgroundColor='#dd2222';
+})
 }
+
